b5fbb85598ea175febaec47641ad76b8ca06a834
Emilio Mayhua <richardkarl@live.com>
2018-11-15 21:16:36 -0500

    nuevos ingresos

---
+++ src/website/empresas.ts
@@ -0,0 +1,24 @@
+import {Router, Request, Response} from "express";
+import validar from "./validar";
+
+const empresas = Router();
+
+empresas.get(["/", "/listar"], [validar.tieneSesion], (req: Request, res: Response) => {
+    console.log("P empresa list: render");
+    res.render("catalogo/empresa", {
+        titulo: "Listado de empresas"
+    });
+});
+
+empresas.get("/ver/:id(\\d+)", [validar.tieneSesion], (req: Request, res: Response) => {
+    console.log("P empresa id: render");
+    res.render("catalogo/empresainfo", {
+        titulo: "Empresas",
+        empresaId: req.params.id
+    });
+});
+/*
+empresas.get("/:id", [validar.tieneSesion], (req: Request, res: Response) => {
+});*/
+
+export default empresas;
