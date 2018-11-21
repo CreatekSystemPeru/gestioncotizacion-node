0ebf0016eda85d62e2b171d865d2983c76ba76f4
Mark Anthony <markanthonyjc@gmail.com>
2018-11-21 15:10:30 -0500

    Next

--- src/mysql/mysql.ts
+++ src/mysql/mysql.ts
@@ -1,87 +1,105 @@
 import mysql = require('mysql');
 
 export default class MySQL {
-    private static _instance: MySQL;
+    // private static _instance: MySQL;
 
-    cnx: mysql.Connection;
-    conectado: boolean = false;
+    // cnx: mysql.Connection;
+    // conectado: boolean = false;
+
+    private static cnx: mysql.Connection;
 
     constructor() {
-        console.log('Clase inicializada');
-        this.cnx = mysql.createConnection({
-            host: 'mi3-wts6.a2hosting.com',
-            user: 'amini_sa',
-            password: 'Createk.2018',
-            database: 'aminias1_bdcotizacion'
-        });
-        this.conectarDB();
-        this.errorConexionDB();
+        // this.conectarDB();
     }
 
-    public static get instance() {
-        //return this._instance || (this._instance = new this());
-        return this._instance = new this();
-    }
+    // public static get instance() {
+    //     return this._instance || (this._instance = new this());
+    // }
 
-    static ejecutarQuery(query: string, callback: Function) {
-        this.instance.cnx.query(query, (err, results: Object[], fields) => {
-            if (err) {
-                console.log('Error en ejecutarQuery(): ', err);
-                return callback(err);
-            }
+    // static ejecutarQuery(query: string, callback: Function) {
+        // this.instance.cnx.query(query, (err, results: Object[], fields) => {
+        //     if (err) {
+        //         console.log('Error en ejecutarQuery(): ', err);
+        //         return callback(err);
+        //     }
 
-            callback(null, results);
-            
-            this.instance.cnx.end();
-        });
-    }
+        //     callback(null, results);
+        // });
+    // }
 
-    static ejecutarQueryAndValues(query: string, values: any, callback: Function) {
-        this.instance.cnx.query(query, values, (err, results: Object[], fields) => {
-            if (err) {
-                console.log('Error en ejecutarQueryAndValues(): ', err);
-                return callback(err);
-            }
+    // private conectarDB() {
+    //     this.cnx.connect((err: mysql.MysqlError) => {
+    //         if (err) {
+    //             console.log('Error in conectarDB(): ', err.message);                
+    //             return;
+    //         }
 
-            callback(null, results);
-            
-            this.instance.cnx.end();
-        });
-    }
-
-    // private inicializar() {
-    //     this.cnx = mysql.createConnection({
-    //         host: 'mi3-wts6.a2hosting.com',
-    //         user: 'amini_sa',
-    //         password: 'Createk.2018',
-    //         database: 'aminias1_bdcotizacion'
+    //         this.conectado = true;
+    //         console.log('Base de datos online');
     //     });
     // }
 
-    private conectarDB() {
-        this.cnx.connect((err: mysql.MysqlError) => {
+    public static ejecutarQuery(query: string, callback: Function) {
+        this.conectarDB();
+        this.cnx.query(query, (err, results: Object[], fields) => {
             if (err) {
-                console.log('Error in conectarDB(): ', err.message);                
-                return;
+                console.log('Error en query(): ', err);
+                return callback(err);
             }
 
-            this.conectado = true;
-            //console.log('Base de datos online');
+            console.log('> Instrucción ejecutada');
+            callback(null, results);
+
+            this.desconectarDB();
         });
     }
 
-    private errorConexionDB() {
-        this.cnx.on('error', (err) => {            
-            if (err.code == 'PROTOCOL_CONNECTION_LOST') {
-                //console.log('Conexión finalizada. Reiniciando...');
-                console.log('Conexión Finalizada');
-                //this.inicializar();
-                // this.conectarDB();
-                // this.errorConexionDB();
+    public static ejecutarQueryAndValues(query: string, values: any, callback: Function) {
+        this.conectarDB();
+        this.cnx.query(query, values, (err, results: Object[], fields) => {
+            if (err) {
+                console.log('Error en query(): ', err);
+                return callback(err);
+            }
+
+            console.log('> Instrucción ejecutada');
+            callback(null, results);
+
+            this.desconectarDB();
+        });
+    }
+
+    static inicializarDB() {
+        console.log('> Clase inicializada');
+        this.cnx = mysql.createConnection({
+            host: 'mi3-wts6.a2hosting.com',
+            user: 'amini_root',
+            password: 'Createk.2018',
+            database: 'aminias1_bdcotizacion'
+        });
+    }
+
+    static conectarDB() {
+        this.inicializarDB();        
+        this.cnx.connect((err: mysql.MysqlError) => {
+            if (err) {
+                console.log('> Error en connect(): ', err.message);
                 return;
             }
 
-            console.log('Error en errorConexionDB(): ', err);
+            console.log('> Conectado a Base de datos');
+        });
+    }
+
+    static desconectarDB() {
+        this.cnx.end((err: mysql.MysqlError) => {
+            if (err) {
+                console.log('> Error in end(): ', err.message);
+                return;
+            }
+
+            console.log('> Desconectado de Base de datos');
+            this.cnx.destroy();
         });
     }
 }
