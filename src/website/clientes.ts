f893d7f4cb7a046af62d278318f0fd54fef9e8a4
Emilio Mayhua <richardkarl@live.com>
2018-11-29 20:58:56 -0500

    cliente sucursal

--- src/website/clientes.ts
+++ src/website/clientes.ts
@@ -21,8 +21,16 @@ clientes.get("/ver/:id(\\d+)", [validar.tieneSesion], (req: Request, res: Respon
 clientes.get("/sucursal/:id(\\d+)", [validar.tieneSesion], (req: Request, res: Response) => {
     console.log("P cliente sucursal: render");
     res.render("catalogo/clientesucursal", {
-        titulo: "Sucursales de Cliente",
+        titulo: "Listado de Sucursales de Cliente",
         clienteId: req.params.id
+    });
+});
+
+clientes.get("/versucursal/:id(\\d+)", [validar.tieneSesion], (req: Request, res: Response) => {
+    console.log("P cliente ver: render");
+    res.render("catalogo/clientesucursalinfo", {
+        titulo: "Sucursal de Clientes",
+        sucursalClienteId: req.params.id
     });
 });
 
