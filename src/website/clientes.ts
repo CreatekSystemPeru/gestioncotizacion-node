3788aef4e4a574afdbc9ca6e09da406f85d969f9
richardkarl <richardkarl@live.com>
2018-11-30 20:05:23 -0500

    renombrando archivos

--- src/website/clientes.ts
+++ src/website/clientes.ts
@@ -3,6 +3,7 @@ import validar from "./validar";
 
 const clientes = Router();
 
+// listado de clientes
 clientes.get(["/", "/listar"], [validar.tieneSesion], (req: Request, res: Response) => {
     console.log("P cliente list: render");
     res.render("catalogo/clientelista", {
@@ -10,6 +11,7 @@ clientes.get(["/", "/listar"], [validar.tieneSesion], (req: Request, res: Respon
     });
 });
 
+// id del cliente para ver sus detalles
 clientes.get("/ver/:id(\\d+)", [validar.tieneSesion], (req: Request, res: Response) => {
     console.log("P cliente ver: render");
     res.render("catalogo/clienteinfo", {
@@ -18,17 +20,19 @@ clientes.get("/ver/:id(\\d+)", [validar.tieneSesion], (req: Request, res: Respon
     });
 });
 
+// id del cliente, para listar sus sucursales
 clientes.get("/sucursal/:id(\\d+)", [validar.tieneSesion], (req: Request, res: Response) => {
     console.log("P cliente sucursal: render");
-    res.render("catalogo/clientesucursal", {
+    res.render("catalogo/sucursal", {
         titulo: "Listado de Sucursales de Cliente",
         clienteId: req.params.id
     });
 });
 
+// id de sucursal, para ver sus detalles y lista de contactos
 clientes.get("/versucursal/:id(\\d+)", [validar.tieneSesion], (req: Request, res: Response) => {
     console.log("P cliente ver: render");
-    res.render("catalogo/clientesucursalinfo", {
+    res.render("catalogo/sucursalinfo", {
         titulo: "Sucursal de Clientes",
         sucursalClienteId: req.params.id
     });
